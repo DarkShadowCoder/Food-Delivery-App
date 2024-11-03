edda0318ca43368e0b2a871c571662fdfe335583
Blackdot <106665251+DarkShadowCoder@users.noreply.github.com>
2024-11-03 06:50:27 +0100

    Update data.ts

--- data/data.ts
+++ data/data.ts
@@ -28,18 +28,31 @@ export const category=[
 
 export const food= [
     {
-
+        id:1,
+        name: "Fied Chicken",
+        price: "13.50",
+        note: "4.1",
+        img: require("@/assets/images/fried_chicken.webp")
     },
     {
-
+        id:2,
+        name: "Takos",
+        price: "5.00",
+        note: "4.0",
+        img: require("@/assets/images/takos.jpg")
     },
     {
-
+        id:3,
+        name: "French fries",
+        price: "8.00",
+        note: "2.9",
+        img: require("@/assets/images/french_fries.jpg")
     },
     {
-
-    },
-    {
-
+        id:4,
+        name: "Pancakes",
+        price: "4.25",
+        note: "4.1",
+        img: require("@/assets/images/pankakes.webp")
     }
 ]
