d42a50177597590b054a6ed0aa5c80f5185a506b
Blackdot <106665251+DarkShadowCoder@users.noreply.github.com>
2024-11-04 10:23:55 +0100

    Update router.d.ts

--- .expo/types/router.d.ts
+++ .expo/types/router.d.ts
@@ -6,7 +6,7 @@ export * from 'expo-router';
 declare module 'expo-router' {
   export namespace ExpoRouter {
     export interface __routes<T extends string = string> extends Record<string, unknown> {
-      StaticRoutes: `/Navigation/TabNavigation` | `/Screens/CartScreen/CartScreen` | `/Screens/DeliveryScreen/DeliveryScreen` | `/Screens/GetStartedScreen/GetStartedScreen` | `/Screens/HomeScreen/HomeScreen` | `/Screens/ProfileScreen/ProfileScreen` | `/Screens/layout` | `/Screens/productScreen/ProductScreen` | `/_sitemap`;
+      StaticRoutes: `/..\hooks\ApolloClientSetup` | `/Navigation/TabNavigation` | `/Screens/CartScreen/CartScreen` | `/Screens/DeliveryScreen/DeliveryScreen` | `/Screens/GetStartedScreen/GetStartedScreen` | `/Screens/HomeScreen/HomeScreen` | `/Screens/ProfileScreen/ProfileScreen` | `/Screens/layout` | `/Screens/productScreen/ProductScreen` | `/_sitemap`;
       DynamicRoutes: never;
       DynamicRouteTemplate: never;
     }
